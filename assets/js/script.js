e9abf317af41708e75c5ff9f74327be96036ddb9
Michael Frayne <fraynemichael300@gmail.com>
2023-01-26 14:44:10 -0700

    stuck on textArea value

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,8 @@ let cTime = document.querySelector('#currentTime')
 let hour = document.querySelector('#hour')
 let now = dayjs();
 let currentHour = now.format('H')
-
+let textArea = $('.description')
+let saveBtn = $('.saveBtn')
 /* // UpdateLocale adds .updateLocale API to update a locale's properties.
 let updateLocale = require('dayjs/plugin/updateLocale')
 dayjs.extend(updateLocale)
@@ -22,27 +23,24 @@ dayjs.updateLocale('en', {
 
 
 $(function () {
+  refreshColor()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time
-  
+  $(saveBtn).click(function(){
+    console.log($(this).parent().find(textArea).slice('value'))
+  })
+  // jquery this  element
   function refreshColor() {
     $('.time-block').each(function(){
       // console.log(this.id + ' :id = ' + currentHour)
       if (this.id == currentHour) {
         $(this).removeClass('past future').addClass('present');
       }else if (this.id < currentHour){
-        $(this).addClass('past');
+        $(this).removeClass('future present').addClass('past');
       }else {
         $(this).removeClass('past present').addClass('future');
       }
